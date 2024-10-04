144a36dfa068cd17ba543de705f534b824311428
Jewel-190237 <190237@ku.ac.bd>
2024-10-05 00:04:09 +0600

    forget password updated

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ require('dotenv').config();
 const cors = require('cors');
 const jwt = require('jsonwebtoken');
 const PORT = process.env.PORT || 5000;
+const nodemailer = require('nodemailer');
 
 // MiddleWare
 app.use(cors());
@@ -84,6 +85,7 @@ async function run() {
                 }
 
                 const token = jwt.sign({ id: user._id, role: user.role }, process.env.JWT_SECRET, { expiresIn: '1h' });
+                userToken = token;
                 res.status(200).send({ message: 'Login successful', token });
             } catch (error) {
                 res.status(500).send({ message: 'Login failed', error });
@@ -109,11 +111,90 @@ async function run() {
         // delete a specific user
         app.delete('/users/:id', verifyJWT, verifyAdmin, async (req, res) => {
             const id = req.params.id;
-            console.log('Deleting user with ID:', id);  
+            console.log('Deleting user with ID:', id);
             const query = { _id: new ObjectId(id) };
             const result = await userCollections.deleteOne(query);
             res.send(result);
         })
+
+        // email verification
+        app.post('/forgetPassword', async (req, res) => {
+            const { phone, email } = req.body;
+
+            try {
+                const existingUser = await userCollections.findOne({ phone });
+
+                if (!existingUser) {
+                    return res.status(404).send({ message: 'User not found' });
+                }
+
+                //sent email
+                var transporter = nodemailer.createTransport({
+                    service: 'gmail',
+                    auth: {
+                        user: '190237@ku.ac.bd',
+                        pass: 'afio mvyu nrrc urkv'
+                    }
+                });
+                const token = jwt.sign({ id: existingUser._id, role: existingUser.role }, process.env.JWT_SECRET, { expiresIn: '5m' });
+                var mailOptions = {
+                    from: '190237@ku.ac.bd',
+                    to: email,
+                    subject: 'Reset Password',
+                    text: `http://localhost:5173/resetPassword/${token}`
+                };
+
+                transporter.sendMail(mailOptions, function (error, info) {
+                    if (error) {
+                        console.log(error);
+                    } else {
+                        console.log('Email sent: ' + info.response);
+                    }
+                });
+
+                res.status(200).send({ message: 'User found', email: email });
+            } catch (error) {
+                res.status(500).send({ message: 'Error while searching for user', error });
+            }
+        });
+
+        // reset password
+        app.post('/resetPassword', async (req, res) => {
+            const { token, newPassword } = req.body;
+
+            try {
+                jwt.verify(token, process.env.JWT_SECRET, async (err, decoded) => {
+                    if (err) {
+                        return res.status(400).send({ message: 'Invalid or expired token' });
+                    }
+
+                    const user = await userCollections.findOne({ _id: new ObjectId(decoded.id) });
+
+                    if (!user) {
+                        return res.status(404).send({ message: 'User not found' });
+                    }
+
+                    const result = await userCollections.updateOne(
+                        { _id: new ObjectId(user._id) },
+                        { $set: { password: newPassword } }
+                    );
+
+                    if (result.modifiedCount === 1) {
+                        return res.status(200).send({ message: 'Password updated successfully' });
+                    } else {
+                        return res.status(500).send({ message: 'Failed to update password' });
+                    }
+                });
+            } catch (error) {
+                res.status(500).send({ message: 'Error while resetting password', error });
+            }
+        });
+
+
+
+
+
+
 
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
